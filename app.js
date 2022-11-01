0540ca95aa6f378337c77c4850c88e2dba358458
Bengisu Tepe <bengisutepe@sabanciuniv.edu>
2022-11-01 20:29:41 +0300

    edit profile , user profile add

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const {check, validationResult} = require("express-validator");
+const Joi = require("joi");
 
 
 
@@ -13,22 +14,59 @@ const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const urlencodedParser = bodyParser.urlencoded({extended: false})
 
-mongoose.connect("mongodb://localhost:27017/tl_userDB");
+
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+const url = `mongodb+srv://bengisutepe:EFqoy3lDdvVodrPE@cluster0.emaofpz.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect("mongodb+srv://bengisutepe:EFqoy3lDdvVodrPE@cluster0.emaofpz.mongodb.net/?retryWrites=true&w=majority",connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
+
+//mongoose.connect("mongodb+srv://bengisutepe:EFqoy3lDdvVodrPE@cluster0.emaofpz.mongodb.net/?retryWrites=true&w=majority");
+
+
+//mongoose.connect("mongodb://localhost:27017/tl_userDB");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
+  username:{
+    type: String,
+    min:3,
+    unique: true,
+    required: [true,"Please check your data entry, no name specified"],
+
+  },
+    email: {
+      type: String,
+      min:3,
+      unique: true,
+      required: [true,"Please check your data entry, no email specified"],
+    },
+    password: {
+      type: String,
+      min:3,
+      unique: true,
+      required: [true,"Please check your data entry, no password specified"],
+    },
+
 
 });
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret},['password'] );
+//const secret = "Thisisourlittlesecret.";
+//userSchema.plugin(encrypt, {secret: secret},['password'] );
 
 const User = new mongoose.model("User",userSchema);
 
@@ -43,12 +81,23 @@ app.get("/login",function(req, res)
 {
   res.render("login");
 });
-app.post("/register",function(req,res){
+
+app.get("/editprofile",function(req, res)
+{
+  res.render("editprofile");
+});
+
+app.get("/profilePage",function(req, res)
+{
+  res.render("profilePage");
+});
+//POST
+app.post("/register",async(req,res)=>{
+  console.log("inside post funct");
   const newUser= new User({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-
   });
   newUser.save(function(err){
     if(err){
