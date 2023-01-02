a5881d314cfb37e656655b925348e7830d65aeb9
Beyza <armagannbeyza@gmail.com>
2023-01-02 16:25:18 +0300

    deployment

--- app.js
+++ app.js
@@ -1054,13 +1054,13 @@ app.post("/informationEditScout", async (req, res) => {
 
 
 
-/*
+
 let port = process.env.PORT;
 if (port == null || port == "") {
 port = 3000;
 }
 app.listen(port);
-*/
+
 app.listen(3000, function () {
  console.log("server on 3000");
 });
