ccc193c1639fd6b9ceb17902c42ff65a307781eb
busrahadimlioglu <busrahadimlioglu@gmail.com>
2023-01-04 13:39:54 +0300

    update port

--- app.js
+++ app.js
@@ -1305,11 +1305,11 @@ app.post("/informationEditScout", async (req, res) => {
 
 
 
-//let port = process.env.PORT;
-//if (port == null || port == "") {
-//port = 3000;
-//}
-//app.listen(port);
+let port = process.env.PORT;
+if (port == null || port == "") {
+port = 3000;
+}
+app.listen(port);
 
 app.listen(3000, function () {
  console.log("server on 3000");
